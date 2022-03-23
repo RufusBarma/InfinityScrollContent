1cc0246ef832a0881b0444e0a5c20048bf2b2df2
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-03-23 16:14:43 +0500

    Init content aggregator development

---
+++ СontentAggregator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Reddit;
+using Reddit.Controllers;
+
+var appId = GetVariable("app_id_reddit");
+var refreshToken = GetVariable("refresh_token_reddit");
+var appSecret = GetVariable("app_secret_reddit");
+var accessToken = GetVariable("access_token_reddit");
+
+var reddit = new RedditClient(appId, refreshToken, appSecret, accessToken);
+
+var subreddit = reddit.Subreddit("all", over18: true);
+List<Post> posts = subreddit.Search();
+
+string GetVariable(string variable)
+{
+	var envVariable = Environment.GetEnvironmentVariable(variable);
+	if (envVariable == null)
+	{
+		throw new Exception($"Can't get '{variable}' from environment!");
+	}
+
+	return envVariable;
+}
