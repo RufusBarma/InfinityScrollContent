cf4403530e2ac4e395a5897cb72223b3c85d57ff
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-03-30 17:16:05 +0500

    Refactoring

--- СontentAggregator/Aggregators/Reddit/RedditAggregator.cs
+++ СontentAggregator/Aggregators/Reddit/RedditAggregator.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using LanguageExt;
+using LanguageExt.SomeHelp;
+using LanguageExt.UnsafeValueAccess;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using Reddit;
@@ -38,7 +41,7 @@ public class RedditAggregator: IAggregator
 	public void Start()
 	{
 		var categoryItems = _categories.SelectMany(GetAllItems).ToList();
-		foreach (var category in categoryItems.Skip(120).Take(20).Select(categoryItem => categoryItem.Title))
+		foreach (var category in categoryItems.Take(1).Select(categoryItem => categoryItem.Title))
 		{
 			var position = _positions.Find(pos => pos.Title == category).FirstOrDefault();
 			if (position == null)
@@ -46,49 +49,34 @@ public class RedditAggregator: IAggregator
 				position = new CategoryPosition {Title = category};
 				_positions.InsertOne(position);
 			}
-			var name = category.Substring(3);
-			Subreddit subreddit;
-			try
-			{
-				subreddit = _reddit.Subreddit(name, over18: true).About();
-			}
-			catch (RedditNotFoundException ex)
-			{
-				_logger.LogError(ex, ex.Message);
-				continue;
-			}
-			if (subreddit.Created == default)
-			{
-				_logger.LogWarning($"{category} is banned or not exist");
-				break;
-			}
 			if (position.AfterEnd)
 			{
 				_logger.LogWarning($"{category} is end");
+				//TODO replace on fetching before
 				continue;
 			}
+			var sub = GetSubreddit(_reddit, category, _logger);
+			if (sub.IsNone)
+				continue;
+			var subreddit = sub.ValueUnsafe();
 			var posts = subreddit.Posts.GetTop(limit: 100, after: position.After);
-			var newAfter = posts.Last().Fullname;
-			if (position.After == newAfter)
+			if (!posts.Any())
 			{
 				_logger.LogInformation($"{category} is end");
 				position.AfterEnd = true;
+				continue;
 			}
 			else
 			{
+				var newAfter = posts.Last().Fullname;
 				position.After = newAfter;
 				_positions.ReplaceOne(pos => pos.Title == position.Title, position, new ReplaceOptions {IsUpsert = true});
 			}
-			var links = posts.Select(post =>
-			{
-				var url = post switch
-				{
-					LinkPost linkPost => linkPost.URL,
-					SelfPost selfPost => selfPost.About().SelfText,
-					_ => throw new ArgumentOutOfRangeException(nameof(post), post, null)
-				};
-				return new Link {Category = category, Url = url};
-			});
+
+			var links = posts
+				.Where(post => post is LinkPost)
+				.Cast<LinkPost>()
+				.Select(post => new Link {Category = category, Url = post.URL});
 			_linkCollection.InsertMany(links);
 		}
 		_logger.LogInformation("Finish RedditParser");
@@ -97,5 +85,20 @@ public class RedditAggregator: IAggregator
 	public void Stop()
 	{
 		throw new NotImplementedException();
+	}
+
+	private Option<Subreddit> GetSubreddit(RedditClient client, string category, ILogger logger)
+	{
+		try
+		{
+			var name = category.Substring(3);
+			return client.Subreddit(name, over18: true).About();
+		}
+		catch (Exception ex)
+		{
+			ex.Data.Add("Subreddit", category);
+			logger.LogWarning(ex, "Exception on get subreddit");
+			return Option<Subreddit>.None;
+		}
 	}
 }
