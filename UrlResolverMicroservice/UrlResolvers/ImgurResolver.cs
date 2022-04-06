f1240dc89d6881a79382808b536f86c2e86cc22a
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-04-06 16:49:19 +0500

    Add imgur url resolver

--- UrlResolverMicroservice/UrlResolvers/ImgurResolver.cs
+++ UrlResolverMicroservice/UrlResolvers/ImgurResolver.cs
@@ -1,17 +1,25 @@
+using System.Net;
 using LanguageExt;
 using Microsoft.Extensions.Configuration;
 using MoreLinq.Extensions;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace UrlResolverMicroservice.UrlResolvers;
 
 public class ImgurResolver: IUrlResolver
 {
-	private Dictionary<string, Func<string, string>> _endpoints = new()
+	private Dictionary<string, (Func<string, string> getEndpoint, Func<JObject, string[]> getUrls)> _endpoints = new()
 	{
-		{"", hash => $"https://api.imgur.com/3/image/{hash}"},
-		{"a", hash => $"https://api.imgur.com/3/album/{hash}/images"},
-		{"gallery", hash => $"https://api.imgur.com/3/gallery/album/{hash}"}
+		{"", (hash => $"https://api.imgur.com/3/image/{hash}", data => new []{data["data"]["link"].Value<string>()})},
+		{"a", (hash => $"https://api.imgur.com/3/album/{hash}/images", 
+			data => data["data"].Children()
+				.Select(image => image.Value<string>("mp4") ?? image.Value<string>("link"))
+				.ToArray())},
+		{"gallery", (hash => $"https://api.imgur.com/3/gallery/album/{hash}", 
+			data => data["data"]["link"]["images"]
+				.Select(image => image["link"].Value<string>())
+				.ToArray())}
 	};
 
 	private readonly RestRequest _request;
@@ -26,13 +34,18 @@ public class ImgurResolver: IUrlResolver
 
 	public async Task<Either<string, string[]>> ResolveAsync(string url)
 	{
-		var urlParts = url.Split('/').SkipUntil(part => part != "imgur.com");
-		var client = new RestClient("https://api.imgur.com/3/album/{{albumHash}}/images")
+		var urlParts = url.Split('/').SkipUntil(part => part == "imgur.com").ToList();
+		var endpointFuncs = urlParts.Count > 1 ? _endpoints[urlParts[0]] : _endpoints[""];
+		var client = new RestClient(endpointFuncs.getEndpoint(urlParts.Last()))
 		{
 			Timeout = -1
 		};
 		var response = await client.ExecuteGetTaskAsync(_request);
-		throw new NotImplementedException();
+		if (response.StatusCode is HttpStatusCode.NotFound or HttpStatusCode.Gone)
+			return response.StatusDescription;
+		var data = JObject.Parse(response.Content);
+		var urls = endpointFuncs.getUrls(data);
+		return urls.Any()? urls: "Empty collection";
 	}
 
 	public bool CanResolve(string url) => url.Contains("imgur.com");
