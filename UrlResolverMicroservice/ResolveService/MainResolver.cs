25edc115469e5210505573a8ac2771f898952194
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-05-05 17:04:42 +0500

    Trying to divide main resolver on small functions

--- UrlResolverMicroservice/ResolveService/MainResolver.cs
+++ UrlResolverMicroservice/ResolveService/MainResolver.cs
@@ -27,6 +27,7 @@ public class MainResolver : IMainResolver
 
 	public async Task Start(CancellationToken cancellationToken)
 	{
+		// _urlResolvers.Select(urlResolver => new TransformManyBlock<string, >())
 		var filter = Builders<Link>.Filter.And(Builders<Link>.Filter.In("Urls", new[] { null, Array.Empty<string>() }),
 			Builders<Link>.Filter.Or(
 				Builders<Link>.Filter.Eq(link => link.ErrorMessage, string.Empty),
@@ -60,6 +61,37 @@ public class MainResolver : IMainResolver
 			await _linkCollection.UpdateOneAsync(updateFilter, update, new UpdateOptions(){IsUpsert = true});
 		}
 		_logger.LogInformation("Resolve completed");
+	}
+
+	private Task<IEnumerable<(Link link, Either<string, string[]>)>> ResolveUrl(IEnumerable<Link> links)
+	{
+		return Task.WhenAll(links.Select(async link => (link, await GetUrlsAsync(link.SourceUrl))));
+	}
+
+	private async Task UpdateLinksInDb(IEnumerable<(Link link, Either<string, string[]> urlsEither)> resolvedLinks)
+	{
+		foreach (var (link, urlsEither) in resolvedLinks)
+		{
+			var skipFlag = false;
+			var update = urlsEither.Match(urls => Builders<Link>.Update
+					.Set(updLink => updLink.Urls, urls)
+					.Set(updLink => updLink.Type, GetLinkType(urls.FirstOrDefault()))
+					.Set(updLink => updLink.IsGallery, urls.Length > 1),
+				error =>
+				{
+					if (error == "Skip")
+						skipFlag = true;
+					return Builders<Link>.Update.Set(updLink => updLink.ErrorMessage, error);
+				});
+			if (skipFlag)
+			{
+				_logger.LogInformation("Skip {link.SourceUrl}", link.SourceUrl);
+				continue;
+			}
+			_logger.LogInformation("Resolved {link.SourceUrl}", link.SourceUrl);
+			var updateFilter = Builders<Link>.Filter.Eq("_id", link._id);
+			await _linkCollection.UpdateOneAsync(updateFilter, update, new UpdateOptions(){IsUpsert = true});
+		}
 	}
 
 	private async Task<Either<string, string[]>> GetUrlsAsync(string url)
