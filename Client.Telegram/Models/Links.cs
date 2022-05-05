2af179bc9818075e8e2802c197fb8c699b94b629
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-05-05 15:39:43 +0500

    Remove from link model unused properties

--- Client.Telegram/Models/Links.cs
+++ Client.Telegram/Models/Links.cs
@@ -1,26 +1,14 @@
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Client.Telegram.Models;
 
-public enum LinkType
-{
-	None,
-	Video,
-	Img,
-	Gif
-}
-
 [BsonIgnoreExtraElements]
 public record Link
 {
-	public ObjectId _id { get; init; }
 	public string SourceUrl { get; init; }
 	public string PermaLink { get; init; }
 	public string Description { get; init; }
 	public string[] Urls { get; set; }
-	public LinkType Type { get; set; }
-	public bool IsGallery { get; set; }
 	public string ErrorMessage { get; set; }
 	public int UpVotes { get; init; }
 	public string[] Category { get; init; }
