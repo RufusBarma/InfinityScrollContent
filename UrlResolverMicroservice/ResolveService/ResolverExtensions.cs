1d400399e60e58d3806ab347ffc15e6edac6eec5
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-05-13 15:22:28 +0500

    Add awaiting cancellation

---
+++ UrlResolverMicroservice/ResolveService/ResolverExtensions.cs
@@ -0,0 +1,23 @@
+using MongoDB.Driver;
+using UrlResolverMicroservice.Models;
+
+namespace UrlResolverMicroservice.ResolveService;
+
+public static class ResolverExtensions
+{
+	public static IEnumerable<Link> GetEmptyUrls(this IMongoCollection<Link> collection)
+	{
+		var filter = Builders<Link>.Filter.And(Builders<Link>.Filter.In("Urls", new[] { null, Array.Empty<string>() }),
+			Builders<Link>.Filter.Or(
+				Builders<Link>.Filter.Eq(link => link.ErrorMessage, string.Empty),
+				Builders<Link>.Filter.Exists(link => link.ErrorMessage, false)));
+		return collection.Find(filter).ToEnumerable();
+	}
+
+	public static Task WhenCanceled(this CancellationToken cancellationToken)
+	{
+		var tcs = new TaskCompletionSource<bool>();
+		cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+		return tcs.Task;
+	}
+}
