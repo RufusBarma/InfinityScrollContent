4b7dd1b7f4135720396900391f1586af62e1168d
RufusBarma <neymanyu@gmail.com>
2022-05-14 16:13:19 +0500

    Check urls on exist before sending

--- Client.Telegram/Client/SendJob.cs
+++ Client.Telegram/Client/SendJob.cs
@@ -30,8 +30,8 @@ public class SendJob: IJob
 
 		var limit = 1;
 		var categories = new[] {"General Categories"};
-		var exceptCategories = new[] {"Gay"};
-		var documents = GetLinks(limit, categories, exceptCategories);
+		var exceptCategories = new[] {""};
+		var documents = await (await GetLinks(categories, exceptCategories)).Where(link => link.Urls.Length <= 10).Take(limit).ToListAsync();
 		await foreach (var link in _sender.Send(documents, channelId, channelUserName, context.CancellationToken))
 		{
 			var postedLink = new PostedLink
@@ -45,36 +45,61 @@ public class SendJob: IJob
 		}
 	}
 
-	private List<Link> GetLinks(int limit, IEnumerable<string> categories, string[] exceptCategories)
+	private IAsyncEnumerable<Link> GetLinks(string category, string[] exceptCategories)
+	{
+		var filters = new List<FilterDefinition<LinkWithPosted>>
+		{
+			Builders<LinkWithPosted>.Filter.AnyNin(link => link.Category, exceptCategories),
+			Builders<LinkWithPosted>.Filter.Exists(link => link.Urls),
+			Builders<LinkWithPosted>.Filter.Ne(link => link.Urls, Array.Empty<string>()),
+			Builders<LinkWithPosted>.Filter.Or(
+				Builders<LinkWithPosted>.Filter.Eq(link => link.ErrorMessage, string.Empty),
+				Builders<LinkWithPosted>.Filter.Exists(link => link.ErrorMessage, false))
+		};
+		if (!string.IsNullOrEmpty(category))
+			filters.Add(Builders<LinkWithPosted>.Filter.AnyIn(link => link.Category, new List<string> {category}));
+		var filter = Builders<LinkWithPosted>.Filter.And(filters);
+
+		var documents = _linkCollection
+			.Aggregate()
+			.Lookup<Link, PostedLink, LinkWithPosted>(_postedCollection, fromType => fromType.SourceUrl,
+				targetType => targetType.SourceUrl, output => output.PostedLinks)
+			.Match(link => !link.PostedLinks.Any())
+			.Match(filter)
+			.SortByDescending(link => link.UpVotes)
+			.ToEnumerable()
+			.Cast<Link>()
+			.ToAsyncEnumerable()
+			.WhereAwait(async link =>
+			{
+				var parallelFound = link.Urls.Select(url => IsFound(url)).ToList();
+				await Task.WhenAll(parallelFound);
+				return parallelFound.All(url => url.Result);
+			});
+		return documents;
+	}
+
+	private async Task<IAsyncEnumerable<Link>> GetLinks(IEnumerable<string> categories, string[] exceptCategories)
 	{
 		foreach (var category in categories.Shuffle())
 		{
-			var filter = Builders<LinkWithPosted>.Filter.And(
-			Builders<LinkWithPosted>.Filter.AnyIn(link => link.Category, new List<string>{category}),
-				Builders<LinkWithPosted>.Filter.AnyNin(link => link.Category, exceptCategories),
-				Builders<LinkWithPosted>.Filter.Exists(link => link.Urls),
-				Builders<LinkWithPosted>.Filter.Ne(link => link.Urls, Array.Empty<string>()),
-				Builders<LinkWithPosted>.Filter.Or(
-					Builders<LinkWithPosted>.Filter.Eq(link => link.ErrorMessage, string.Empty),
-					Builders<LinkWithPosted>.Filter.Exists(link => link.ErrorMessage, false)));
-
-			var documents = _linkCollection
-				.Aggregate()
-				.Lookup<Link, PostedLink, LinkWithPosted>(_postedCollection, fromType => fromType.SourceUrl,
-					targetType => targetType.SourceUrl, output => output.PostedLinks)
-				.Match(link => !link.PostedLinks.Any())
-				.Match(filter)
-				.SortByDescending(link => link.UpVotes)
-				.ToEnumerable()
-				.Take(limit)
-				.Cast<Link>()
-				.ToList();
-			if (!documents.Any())
+			var documents = GetLinks(category, exceptCategories);
+			if (!await documents.AnyAsync())
 				_logger.LogWarning($"Documents count is 0 for {category}");
 			else
 				return documents;
 		}
-		_logger.LogWarning("All categories is empty");
-		return new List<Link>();
+
+		var documentsWithoutCategory = GetLinks(string.Empty, exceptCategories);
+		if (!await documentsWithoutCategory.AnyAsync())
+			_logger.LogWarning("All categories is empty");
+		return documentsWithoutCategory;
+	}
+
+	private async Task<bool> IsFound(string url)
+	{
+		var client = new HttpClient(new HttpClientHandler{AllowAutoRedirect = false});
+		var result = await client.GetAsync(url);
+		return result.IsSuccessStatusCode;
 	}
 }
